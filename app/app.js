a7646e9bf80e9ea2a190c28d3e3b70c7ea1891fc
Soraya <svalleortega@gmail.com>
2021-02-23 21:14:55 +0100

    Add extra functions and style

--- app/app.js
+++ app/app.js
@@ -23,13 +23,34 @@ recognition.addEventListener('result', (e) => {
   let color = e.results[0][0].transcript;
   document.body.style.background = color;
   if (isColor(color)) {
-    output.innerHTML = color;
+    output.innerHTML = `I heard: ${color}`;
   } else {
     output.innerHTML = `Sorry, I didn't understand that color.`
   }
-  
 });
 
 recognition.addEventListener('nomatch', () => output.innerHTML = `Sorry, I didn't understand that color.`);
 recognition.addEventListener('speechend', () => recognition.stop());
 recognition.addEventListener('error', (e) => { output.innerHTML = `Speech recognition error detected: ${e.error}`});
+
+//
+
+const greetingTxt = document.querySelector('.greeting');
+let i = 0;
+const text = `Hello there. Click the button, say a color in English and I'll paint the screen.`;
+
+function typeGreeting() {
+   if (i < text.length) {
+    greetingTxt.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeGreeting, 60);
+  }
+
+  if (i === text.length) {
+    setTimeout(() => {
+      listen.classList.remove('hide');
+    }, 500);
+  }
+}
+
+window.addEventListener('load', typeGreeting);
